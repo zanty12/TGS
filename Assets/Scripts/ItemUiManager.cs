191b80c9daa1892fcaab225abcec333dad912db6
sute-kizamurai <nakayama.hayate4510471@gmail.com>
2025-03-21 12:56:27 +0900

    初期にアイテムを読みこむ用のスクリプト作成

---
+++ Assets/Scripts/ItemUiManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ItemUiManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameObject;
+    [SerializeField] GameObject parent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Instantiate(gameObject, parent.transform);
+    }
+
+    public void SetItem()
+    {
+    }
+}
